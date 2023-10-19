d1dae64c3f04d8df52624944bd37ecb48a6c37b9
GavinNguyen17 <124718536+GavinNguyen17@users.noreply.github.com>
2023-10-19 14:39:10 -0500

    Update Backend.py

--- Backend.py
+++ Backend.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, jsonify
 from flask_cors import cross_origin,CORS
 import logging
-app=Flask(__name__,static_folder='.build',static_url_path='/')
+app=Flask(__name__,static_folder='./build',static_url_path='/')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 #Sign Up Backend
@@ -10,7 +10,12 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 def Hi(User,Pass):
     print(User)
-    successM= {"Name": User, "Code": 200}
+    successM= {"name": User, "Code": 200}
     return jsonify(successM), 200
+
+@app.route('/')
+@cross_origin()
+def index():
+    return app.send_static_file('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, port=5000)
